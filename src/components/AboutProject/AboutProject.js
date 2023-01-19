153360ee06bd3a8fd0da11886dfeda3b3988068b
Denis <den-apple@ya.ru>
2023-01-19 21:41:28 +0200

    chore: стилизовать компоненты AboutProject, Subtitle

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,7 +1,34 @@
+import Subtitle from '../Subtitle/Subtitle';
 import './AboutProject.css';
 
 const AboutProject = () => {
-  return <section className="about">AboutProject</section>;
+  return (
+    <section className="about">
+      <Subtitle text="О проекте" />
+      <div className="about__two-columns">
+        <article className="about__column">
+          <h3 className="about__title">Дипломный проект включал 5 этапов</h3>
+          <h4 className="about__subtitle">
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и финальные доработки.
+          </h4>
+        </article>
+        <article className="about__column">
+          <h3 className="about__title">На выполнение диплома ушло 5 недель</h3>
+          <h4 className="about__subtitle">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </h4>
+        </article>
+      </div>
+      <section className="about__timeline">
+        <p className="about__timeline-weeks">1 неделя</p>
+        <p className="about__timeline-direction">Back-end</p>
+        <p className="about__timeline-weeks">4 недели</p>
+        <p className="about__timeline-direction">Front-end</p>
+      </section>
+    </section>
+  );
 };
 
 export default AboutProject;
