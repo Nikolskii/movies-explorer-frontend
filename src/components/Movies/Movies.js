6547774d95731e5ed9295a5a7e51efbedc65e215
Denis <den-apple@ya.ru>
2023-01-22 14:49:28 +0200

    feat: создать компонент Preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,10 +1,12 @@
 import SearchForm from '../SearchForm/SearchForm';
+import Preloader from '../Preloader/Preloader';
 import './Movies.css';
 
 const Movies = () => {
   return (
     <section className="movies">
       <SearchForm />
+      <Preloader />
     </section>
   );
 };
