92ea5f3e2c2ff0b1c1820f1236f55e499a787944
Denis <den-apple@ya.ru>
2023-01-22 10:54:30 +0200

    fix: исправить отступы между секциями компонента main

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -3,31 +3,35 @@ import './AboutProject.css';
 
 const AboutProject = () => {
   return (
-    <section className="about" id="about-project">
+    <section className="about-project" id="about-project">
       <Subtitle text="О проекте" />
-      <div className="about__two-columns">
-        <article className="about__column">
-          <h3 className="about__title">Дипломный проект включал 5 этапов</h3>
-          <h4 className="about__subtitle">
+      <div className="about-project__two-columns">
+        <article className="about-project__column">
+          <h3 className="about-project__title">
+            Дипломный проект включал 5 этапов
+          </h3>
+          <h4 className="about-project__subtitle">
             Составление плана, работу над бэкендом, вёрстку, добавление
             функциональности и финальные доработки.
           </h4>
         </article>
-        <article className="about__column">
-          <h3 className="about__title">На выполнение диплома ушло 5 недель</h3>
-          <h4 className="about__subtitle">
+        <article className="about-project__column">
+          <h3 className="about-project__title">
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <h4 className="about-project__subtitle">
             У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
             соблюдать, чтобы успешно защититься.
           </h4>
         </article>
       </div>
-      <section className="about__timeline">
-        <p className="about__timeline-weeks about__timeline-weeks_akcent">
+      <section className="about-project__timeline">
+        <p className="about-project__timeline-weeks about-project__timeline-weeks_akcent">
           1 неделя
         </p>
-        <p className="about__timeline-weeks">4 недели</p>
-        <p className="about__timeline-direction">Back-end</p>
-        <p className="about__timeline-direction">Front-end</p>
+        <p className="about-project__timeline-weeks">4 недели</p>
+        <p className="about-project__timeline-direction">Back-end</p>
+        <p className="about-project__timeline-direction">Front-end</p>
       </section>
     </section>
   );
