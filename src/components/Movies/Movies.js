12203457602f5ae8b1aeccdd338b56cc33e4c165
Denis <den-apple@ya.ru>
2023-01-22 16:02:12 +0200

    chore: создать компонент MoviesCardList

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,11 +1,13 @@
 import SearchForm from '../SearchForm/SearchForm';
 import Preloader from '../Preloader/Preloader';
 import './Movies.css';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
 
 const Movies = () => {
   return (
     <section className="movies">
       <SearchForm />
+      <MoviesCardList />
     </section>
   );
 };
---
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -0,0 +1,7 @@
+import './MoviesCardList.css';
+
+const MoviesCardList = () => {
+  return <section className="movies-card-list"></section>;
+};
+
+export default MoviesCardList;
