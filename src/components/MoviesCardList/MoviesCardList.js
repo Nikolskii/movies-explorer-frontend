1ff4875b18ed5066b600a15f2d1210b2ad583bbf
Denis <den-apple@ya.ru>
2023-01-22 17:04:57 +0200

    chore: стилизовать компонент MovieCard

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,7 +1,12 @@
+import MovieCard from '../MovieCard/MovieCard';
 import './MoviesCardList.css';
 
 const MoviesCardList = () => {
-  return <section className="movies-card-list"></section>;
+  return (
+    <section className="movies-card-list">
+      <MovieCard />
+    </section>
+  );
 };
 
 export default MoviesCardList;
