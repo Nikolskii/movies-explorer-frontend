8e2da329935e295e8a4600da772f7823f89dfadf
Denis <den-apple@ya.ru>
2023-01-23 14:49:54 +0200

    feat: стилизовать компонент Profile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
+import Profile from '../Profile/Profile';
 
 const App = () => {
   return (
     <div className="page">
       {/* <Main /> */}
-      <Movies />
+      {/* <Movies /> */}
+      <Profile />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,40 @@
+import './Profile.css';
+
+const Profile = () => {
+  return (
+    <section className="profile">
+      <h1 className="profile__title">Привет, Денис!</h1>
+      <form className="profile-form">
+        <fieldset className="profile-form__fieldset">
+          <div className="profile-form__param">
+            <label className="profile-form__label" for="name">
+              Имя
+            </label>
+            <input
+              className="profile-form__input"
+              type="text"
+              id="name"
+              placeholder="Денис"
+            />
+          </div>
+
+          <div className="profile-form__param">
+            <label className="profile-form__label" for="email">
+              E-mail
+            </label>
+            <input
+              className="profile-form__input"
+              type="text"
+              id="email"
+              placeholder="denis-nikolski@ya.ru"
+            />
+          </div>
+        </fieldset>
+        <button className="profile_form__button">Редактировать</button>
+      </form>
+      <button className="profile__button">Выйти из аккаунта</button>
+    </section>
+  );
+};
+
+export default Profile;
