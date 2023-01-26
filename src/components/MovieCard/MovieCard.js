a15058568c384d79afbd3f22b786fda1e3fc6ca4
Denis <den-apple@ya.ru>
2023-01-26 20:14:46 +0200

    chore: добавить длительность фильма в карточке

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,8 +5,13 @@ const MovieCard = ({ movie }) => {
   return (
     <article className="movie-card">
       <h2 className="movie-card__title">{movie.nameRU}</h2>
-      <p className="movie-card__duration">0 минут </p>
-      <a className="movie-card__link" href={movie.trailerLink} target="_blank">
+      <p className="movie-card__duration">{`${movie.duration} минут`}</p>
+      <a
+        className="movie-card__link"
+        href={movie.trailerLink}
+        target="_blank"
+        rel="noreferrer"
+      >
         <img
           className="movie-card__cover"
           src={`https://api.nomoreparties.co${movie.image.url}`}
