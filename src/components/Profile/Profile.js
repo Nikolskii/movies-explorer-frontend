50998c789c39e042b51d3f7dad546d3dbbeebb13
Denis <den-apple@ya.ru>
2023-02-01 16:32:47 +0200

    feat: сделать валидацию формы компонента Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,6 +2,7 @@ import Header from '../Header/Header';
 import CurrentUserContext from '../../context/CurrentUserContext';
 import './Profile.css';
 import React, { useEffect, useState } from 'react';
+import useFormWithValidation from '../../hooks/useFormWithValidation';
 
 const Profile = ({
   onBurgerMenu,
@@ -11,18 +12,20 @@ const Profile = ({
   updateUserButtonText,
 }) => {
   const currentUser = React.useContext(CurrentUserContext);
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const { values, handleChange, errors, isValid } = useFormWithValidation({});
+  const { name, email } = currentUser;
+  const [isNewData, setIsNewData] = useState(null);
 
   useEffect(() => {
-    setName(currentUser.name);
-    setEmail(currentUser.email);
-  }, [currentUser]);
+    values.name === name && values.email === email
+      ? setIsNewData(false)
+      : setIsNewData(true);
+  }, [name, email, values.name, values.email]);
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
 
-    onUpdateUser({ name, email });
+    onUpdateUser({ name: values.name, email: values.email });
   };
 
   return (
@@ -38,12 +41,19 @@ const Profile = ({
               </label>
               <input
                 className="profile-form__input"
-                name="userName"
+                name="name"
                 type="text"
                 id="name"
-                value={name || ''}
-                onChange={(evt) => setName(evt.target.value)}
+                placeholder={name}
+                value={values.name || ''}
+                onChange={handleChange}
+                pattern="^[А-яёA-z\-\s]{2,30}$"
+                required
               />
+              <span className="profile-form_error">
+                {errors.name &&
+                  'Поле может содержать только латиницу, кириллицу, пробел или дефис от 2 до 30 знаков'}
+              </span>
             </div>
             <div className="profile-form__field">
               <label className="profile-form__label" htmlFor="email">
@@ -51,15 +61,27 @@ const Profile = ({
               </label>
               <input
                 className="profile-form__input"
-                name="userEmail"
-                type="text"
+                name="email"
+                type="email"
                 id="email"
-                value={email || ''}
-                onChange={(evt) => setEmail(evt.target.value)}
+                placeholder={email}
+                value={values.email || ''}
+                onChange={handleChange}
+                pattern="\S+@\S+\.\S+"
+                required
               />
+              <span className="profile-form_error">
+                {errors.email && 'Введен некорреткный e-mail адрес'}
+              </span>
             </div>
           </fieldset>
-          <button type="submit" className="profile_form__button">
+          <button
+            disabled={!isValid || !isNewData}
+            type="submit"
+            className={`profile_form__button ${
+              (!isValid || !isNewData) && 'profile_form__button_inactive'
+            }`}
+          >
             {updateUserButtonText}
           </button>
         </form>
